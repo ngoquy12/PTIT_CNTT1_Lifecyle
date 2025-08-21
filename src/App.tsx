5eded23612be64db3acac5f42909c38f0e294fc1
ngovanquy <ngoq753429@gmail.com>
2025-08-21 08:29:59 +0700

    upadte files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import ControlledForm from "./components/ControlledForm";
+import FormMultipleFiled from "./components/FormMultipleFiled";
+// import ParentComponent from "./components/ParentComponent";
+
+function App() {
+  return (
+    <>
+      {/* <ParentComponent /> */}
+      {/* <ControlledForm /> */}
+      <FormMultipleFiled />
+    </>
+  );
+}
+
+export default App;
