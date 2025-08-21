d0b53c73333d4ca76d09163d43702239a2052f29
ngovanquy <ngoq753429@gmail.com>
2025-08-21 09:34:21 +0700

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import ControlledForm from "./components/ControlledForm";
 import FormMultipleFiled from "./components/FormMultipleFiled";
+import Timer from "./components/timer/Timer";
 // import ParentComponent from "./components/ParentComponent";
 
 function App() {
+  const [isShow, setIsShow] = useState<boolean>(false);
+
   return (
     <>
       {/* <ParentComponent /> */}
       {/* <ControlledForm /> */}
-      <FormMultipleFiled />
+      {/* <FormMultipleFiled /> */}
+      <button onClick={() => setIsShow((prev) => !prev)}>Toggle</button>
+
+      {isShow && <Timer />}
     </>
   );
 }
